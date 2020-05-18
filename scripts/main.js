9415e170c55ce49c3a5a7ceec41f77783c7b0a25
rayjreilly <54003481+rayjreilly@users.noreply.github.com>
2020-05-18 14:11:19 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 // listen for button clicks
 // when they happen, get that API URL
 // then when that comes back, use the data to build <li> elements.
-
+// 
 document.querySelector('#search-button').addEventListener('click', function () {
 	// alert('the button was clicked');
 	fetch('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json').then(function (results) {
