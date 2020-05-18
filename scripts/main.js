d1a5837c6606fa7b2bd02c1f30e3c8dc773f5822
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-05-18 12:52:25 -0700

    button listener added

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// select the button
+// listen for button clicks
+// when they happen, get that API URL
+// then when that comes back, use the data to build <li> elements.
+
+document.querySelector('#search-button').addEventListener('click', function() {
+	alert('the button was clicked');
+});
