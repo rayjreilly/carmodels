4a46506dbdf88bb6e438f54e012f0c46f406348e
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-05-18 13:18:49 -0700

    console all cars

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,20 @@
 // when they happen, get that API URL
 // then when that comes back, use the data to build <li> elements.
 
-document.querySelector('#search-button').addEventListener('click', function() {
+document.querySelector('#search-button').addEventListener('click', function () {
 	// alert('the button was clicked');
-	fetch('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json').then(function(results) {
+	fetch('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json').then(function (results) {
 		return results.json()
-	}).then(function(data) {
-		console.log('data');
+	}).then(function (data) {
 		console.log(data);
+		var carsContainer = document.querySelector('#cars-container');
+
+		var cars = data.Results;
+
+		cars.forEach(function(item, iteration) {
+						// console.log(item.Make_Name)
+						console.log(`The car on iteration ${item.iteration} is ${item.Make_Name}`);
+		});
 	})
-	
+
 }); 
